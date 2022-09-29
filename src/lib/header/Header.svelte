830d25f089528abfd66b78ab4757b9dd6f920db6
Floaty_KT <FloatKasemtan@users.noreply.github.com>
2022-09-29 18:09:53 +0700

    feat: add navbar & config tailwind

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -1,11 +1,23 @@
 <script lang="ts">
-	import { page } from '$app/stores';
+	interface Page {
+		title: string;
+		path: string;
+	}
+	var pages: Page[] = [
+		{ path: '/about', title: 'About' },
+		{ path: '/survey', title: 'Survey' },
+		{ path: '/graph', title: 'Graph' }
+	];
 </script>
 
-<header>
-	
+<header class="flex items-center justify-between px-10">
+	<a href="/" class="font-extrabold p-5 transition-all hover:scale-125">AI in coding</a>
+	<div>
+		{#each pages as page}
+			<a href={page.path} class="p-5 opacity-50 transition-all hover:opacity-100 ">{page.title}</a>
+		{/each}
+	</div>
 </header>
 
 <style>
-
 </style>
