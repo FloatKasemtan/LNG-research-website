4ebb080b6bb7c6b6ec2864c965d12b39a60eb082
Sirawit Pratoomsuwan <s36526@bj.ac.th>
2022-10-02 23:57:28 +0700

    Update Header.svelte

--- src/lib/components/header/Header.svelte
+++ src/lib/components/header/Header.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	interface Page {
 		title: string;
 		path: string;
@@ -9,6 +10,8 @@
 		{ path: '/survey', title: 'Survey' },
 		{ path: '/graph', title: 'Graph' }
 	];
+
+	$: currentPath = $page.url.pathname;
 </script>
 
 <!-- <header class="flex items-center justify-between px-10">
@@ -23,15 +26,23 @@
 <header
 	class="backdrop:blur-sm  z-50 flex h-[4.5rem] items-center gap-2 px-2 md:px-10 left-0 right-0 top-0"
 >
-	<b class="text-gray-800 transition-all"> <a href="/">AI in Coding</a></b>
+	<b class="text-gray-800 transition-all text-xl">
+		<a href="/">LNG320 Artificial Intelligence</a></b
+	>
 	<ul class="flex-1 h-8">
 		{#each pages as page}
 			<li class="font-semibold text-gray-500 relative  h-full group">
-				<a href={page.path} class="p-5  h-full transition-all opacity-50 group-hover:opacity-100 "
+				<a
+					href={page.path}
+					class="{page.path === currentPath
+						? 'font-bold text-gray-900'
+						: ''} p-5 h-full transition-all opacity-50 group-hover:opacity-100 "
 					>{page.title}
 				</a>
 				<div
-					class="transition-all absolute bottom-0 left-0 group-hover:w-10 w-0 h-1 bg-gradient-to-r from-[#ff0080] to-[#7928ca]"
+					class="{page.path === currentPath
+						? 'w-10 '
+						: 'w-0'} transition-all absolute bottom-0 left-0 group-hover:w-10  h-1 bg-gradient-to-r from-[#ff0080] to-[#7928ca]"
 				/>
 			</li>
 		{/each}
