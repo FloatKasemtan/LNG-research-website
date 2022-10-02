55d4a498c4289d108e99f62d8dd59e56cb0b9b6f
Jirasin <jjrsinx@gmail.com>
2022-10-02 17:38:02 +0700

    feat: graph

---
+++ src/lib/graph/data.ts
@@ -0,0 +1,156 @@
+export const data1 = {
+	labels: ['1-3 Years', '3-5 Years', 'More than 5 years', 'Less than 1 year'],
+	datasets: [
+		{
+			data: [58.6, 17.2, 17.2, 6.9],
+			backgroundColor: ['#F7464A', '#46BFBD', '#FDB45C', '#4D5360'],
+			hoverBackgroundColor: ['#FF5A5E', '#5AD3D1', '#FFC870', '#616774']
+		}
+	]
+};
+export const data2 = {
+	labels: ['Yes', 'No'],
+	datasets: [
+		{
+			data: [51.7, 48.3],
+			backgroundColor: ['#790252', '#FFD384'],
+			hoverBackgroundColor: ['#AF0171', '#FFF9B0']
+		}
+	]
+};
+export const data3 = {
+	labels: ['Github Copilot', 'Kite', 'Tabnine', 'OpenAI Codex', 'Grepper'],
+	datasets: [
+		{
+			label: 'No. of people',
+			data: [14, 3, 3, 4, 1],
+			backgroundColor: [
+				'rgba(255, 134,159,0.4)',
+				'rgba(98,  182, 239,0.4)',
+				'rgba(255, 218, 128,0.4)',
+				'rgba(113, 205, 205,0.4)',
+				'rgba(170, 128, 252,0.4)',
+				'rgba(255, 177, 101,0.4)'
+			],
+			borderWidth: 2,
+			borderColor: [
+				'rgba(255, 134, 159, 1)',
+				'rgba(98,  182, 239, 1)',
+				'rgba(255, 218, 128, 1)',
+				'rgba(113, 205, 205, 1)',
+				'rgba(170, 128, 252, 1)',
+				'rgba(255, 177, 101, 1)'
+			]
+		}
+	]
+};
+export const data4 = {
+	labels: ['Less than 3 months', '3 - 6 months', '6 - 12 months', 'More than 1 year'],
+	datasets: [
+		{
+			data: [40, 40, 13.3, 6.7],
+			backgroundColor: [
+				'rgba(255, 134, 159, 1)',
+				'rgba(98,  182, 239, 1)',
+				'rgba(255, 218, 128, 1)',
+				'rgba(113, 205, 205, 1)',
+				'rgba(170, 128, 252, 1)'
+			],
+			hoverBackgroundColor: [
+				'rgba(255, 134, 159, 0.9)',
+				'rgba(98,  182, 239, 0.9)',
+				'rgba(255, 218, 128, 0.9)',
+				'rgba(113, 205, 205, 0.9)',
+				'rgba(170, 128, 252, 0.9)'
+			]
+		}
+	]
+};
+export const data5 = {
+	labels: ['1', '2', '3', '4', '5'],
+	datasets: [
+		{
+			label: '% of people',
+			data: [0, 6.7, 6.7, 60, 26.7],
+			backgroundColor: [
+				'rgba(255, 134,159,0.4)',
+				'rgba(98,  182, 239,0.4)',
+				'rgba(255, 218, 128,0.4)',
+				'rgba(113, 205, 205,0.4)',
+				'rgba(170, 128, 252,0.4)',
+				'rgba(255, 177, 101,0.4)'
+			],
+			borderWidth: 2,
+			borderColor: [
+				'rgba(255, 134, 159, 1)',
+				'rgba(98,  182, 239, 1)',
+				'rgba(255, 218, 128, 1)',
+				'rgba(113, 205, 205, 1)',
+				'rgba(170, 128, 252, 1)',
+				'rgba(255, 177, 101, 1)'
+			]
+		}
+	]
+};
+export const data6 = {
+	labels: ['Yes', 'No', 'Maybe'],
+	datasets: [
+		{
+			data: [60, 13.3, 26.7],
+			backgroundColor: [
+				'rgba(255, 134, 159, 1)',
+				'rgba(98,  182, 239, 1)',
+				'rgba(255, 218, 128, 1)'
+			],
+			hoverBackgroundColor: [
+				'rgba(255, 134, 159, 0.9)',
+				'rgba(98,  182, 239, 0.9)',
+				'rgba(255, 218, 128, 0.9)'
+			]
+		}
+	]
+};
+export const data7 = {
+	labels: ['Github Copilot', 'Kite', 'Tabnine', 'OpenAI Codex', 'Never'],
+	datasets: [
+		{
+			label: '% of people',
+			data: [11, 0, 0, 4, 1],
+			backgroundColor: [
+				'rgba(255, 134,159,0.4)',
+				'rgba(98,  182, 239,0.4)',
+				'rgba(255, 218, 128,0.4)',
+				'rgba(113, 205, 205,0.4)',
+				'rgba(170, 128, 252,0.4)',
+				'rgba(255, 177, 101,0.4)'
+			],
+			borderWidth: 2,
+			borderColor: [
+				'rgba(255, 134, 159, 1)',
+				'rgba(98,  182, 239, 1)',
+				'rgba(255, 218, 128, 1)',
+				'rgba(113, 205, 205, 1)',
+				'rgba(170, 128, 252, 1)',
+				'rgba(255, 177, 101, 1)'
+			]
+		}
+	]
+};
+export const data8 = {
+	labels: ['Yes', 'No', 'Maybe'],
+	datasets: [
+		{
+			data: [71.4, 0, 28.6],
+			backgroundColor: [
+				'rgba(98,  182, 239, 1)',
+				'rgba(255, 134, 159, 1)',
+				'rgba(255, 218, 128, 1)'
+			],
+			hoverBackgroundColor: [
+				'rgba(98,  182, 239, 0.9)',
+				'rgba(255, 134, 159, 0.9)',
+				'rgba(255, 218, 128, 0.9)'
+			]
+		}
+	]
+};
